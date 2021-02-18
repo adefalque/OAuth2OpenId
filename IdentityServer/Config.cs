2e733c2df32a8c0c056369db9cc656ee65feff05
Alexandre Defalque <adefalque@efront.com>
2021-02-18 19:46:07 +0100

    Intial commit

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+using IdentityServer4;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email()
+            };
+
+        public static IEnumerable<ApiResource> ApiResources =>
+            new[]
+            {
+                new ApiResource("weatherforecast", "Weather Forecast Api")
+                {
+                    Scopes = new[] { "weatherforecast.accessall" }
+                },
+            };
+        
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new[]
+            {
+                new ApiScope("weatherforecast.accessall"),
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new[]
+            {
+                new Client
+                {
+                    ClientId = "postmanclientcreds",
+                    ClientName = "Postman Client Credentials Client",
+                    ClientSecrets = new[] { new Secret("sW0Y8Mji8793MmJDLsDI1eOM3aUWCmWO".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = new[] { "weatherforecast.accessall" }
+                },
+                new Client
+                {
+                    ClientId = "mvcappcodeflow",
+                    ClientName = "Mvc App Code Flow Client",
+                    ClientSecrets = new[] { new Secret("sW0Y8Mji8793MmJDLsDI1eOM3aUWCmWO".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = new List<string>() { "https://localhost:5011/signin-oidc" },
+                    PostLogoutRedirectUris = new List<string>() { "https://localhost:5011/signout-callback-oidc" },
+                    AllowedScopes = new[]
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email
+                    },
+                    RequirePkce = true,
+                    RequireConsent = true
+                }
+            };
+    }
+}
---
+++ MvcApp/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using MvcApp.Models;
+
+namespace MvcApp.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            this.logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var idToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.IdToken);
+            logger.LogInformation(idToken);
+
+            foreach (var claim in User.Claims)
+            {
+                logger.LogInformation($"[{claim.Type}] = {claim.Value}");
+            }
+            
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
